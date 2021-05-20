d64353dba18a21b6e5d365a272903d6ffd8b25ff
theOnlyihao <62514637+theOnlyi@users.noreply.github.com>
2021-05-20 15:34:23 -0400

    First React App

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -10,15 +10,20 @@ export class Search extends Component {
     searchUsers: PropTypes.func.isRequired,
     clearUsers: PropTypes.func.isRequired,
     showClear: PropTypes.bool.isRequired,
+    setAlert: PropTypes.func.isRequired,
   };
 
   onChange = (e) => this.setState({ [e.target.name]: e.target.value });
 
   onSubmit = (e) => {
     e.preventDefault();
+    if (this.state.text === "") {
+      this.props.setAlert("Please enter somthing", "light");
+    } else {
+      this.props.searchUsers(this.state.text);
+      this.setState({ text: "" });
+    }
     //console.log(this.state.text);
-    this.props.searchUsers(this.state.text);
-    this.setState({ text: "" });
   };
 
   render() {
